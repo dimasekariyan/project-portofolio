8fe89fdc3de940828f0dd3f52f2c16b283b85784
dimasekariyan <dimaseka0024@gmail.com>
2022-11-20 02:34:26 +0700

    fifth commit

--- app/Http/Controllers/authController.php
+++ app/Http/Controllers/authController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
 use Laravel\Socialite\Facades\Socialite;
 
 class authController extends Controller
@@ -23,13 +24,22 @@ class authController extends Controller
         $id = $user->id;
         $email = $user->email;
         $name = $user->name;
+        $avatar = $user->avatar;
+
+        
 
         $cek = User::where('email', $email)->count();
         if($cek > 0) {
+            $avatar_file = $id . ".jpg";
+            $fileContent = file_get_contents($avatar);
+            File::put(public_path("admin/images/faces/$avatar_file"), $fileContent);
+
             $user = User::updateOrCreate(
                 ['email' => $email],
-                ['name' => $name,
-                'google_id' => $id
+                [
+                'name' => $name,
+                'google_id' => $id,
+                'avatar' => $avatar_file
                 ],
             );
             
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\authController;
 use Laravel\Socialite\Facades\Socialite;
+use App\Http\Controllers\halamanController;
 
 
 /*
@@ -27,6 +28,9 @@ Route::get('/auth/redirect', [authController::class,"redirect"])->middleware('is
 Route::get('/auth/callback', [authController::class,"callback"])->middleware('isTamu');
 Route::get('/auth/logout',[authController::class,"logout"]);
 
-Route::get('/dashboard', function () {
-    return 'Selamat Datang '.Auth::User()->email .' di halaman Dashboard';
-})->middleware('auth');
+Route::prefix('dashboard')->middleware('auth')->group(
+    function () {
+        Route::get('/', [halamanController::class, 'index']);
+        Route::resource('halaman', halamanController::class);
+    }
+);
